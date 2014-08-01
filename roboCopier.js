74e68e62be1995a37ca7da88e6a3abbba8dfed8e
Gianpaulo <jam_pow@hotmail.com>
2014-08-01 20:26:06 -0300

    adicionando eventos ao módulo de conexão com webdav

--- roboCopier.js
+++ roboCopier.js
@@ -1,8 +1,18 @@
 var connect = require('./utils/connect.js');
+var exec = require( 'child_process' ).exec;
 
+connect.on( 'conectou', function( path ){
+	exec( 'cat ~/webmount/noticias/index.htm', function( error, stdout, stderr ){
+		console.log( stdout );
+		connect.umount( '~/webmount/noticias' );
+	});
+
+});
+
+connect.on( 'desconectou', function( path ){
+	console.log( 'saiu de ' + path );
+});
 
 connect.mount( '~/webmount/noticias' );
 
 
-
-// connect.umount( 'noticias' );
--- utils/connect.js
+++ utils/connect.js
@@ -1,21 +1,44 @@
-var WebDavMount = function(){};
 var exec = require( 'child_process' ).exec;
+var events = require( 'events' );
+var WebDavMount = function(){ events.EventEmitter.call( this ); };
 
+// extendendo EventEmitter
+WebDavMount.super_ = events.EventEmitter;
+WebDavMount.prototype = Object.create( events.EventEmitter.prototype, {
+	constructor: {
+		value: WebDavMount,
+		enumerable: false
+	}
+});
+
+// Monta o path recebido por parâmetro
 WebDavMount.prototype.mount = function( path ) {
+	var self = this;
 	console.log( 'conectando a ' + path );
 	exec( 'mount ' + path, function( error, stdout, stderr ){
+		console.log( '----------------------------------error----------------------------------' );
 		console.log( error );
+		console.log( '----------------------------------stdout----------------------------------' );
 		console.log( stdout );
+		console.log( '----------------------------------stderr----------------------------------' );
 		console.log( stderr );
+
+		self.emit( 'conectou', path );
 	});
 }
 
+// Desmonta o path recebido por parâmetro
 WebDavMount.prototype.umount = function( path ) {
+	var self = this;
 	console.log( path ? 'desmontando ' + path : 'nome não passado' );
 	exec( 'umount ' + path, function( error, stdout, stderr ){
+		console.log( '----------------------------------error----------------------------------' );
 		console.log( error );
+		console.log( '----------------------------------stdout----------------------------------' );
 		console.log( stdout );
+		console.log( '----------------------------------stderr----------------------------------' );
 		console.log( stderr );
+		self.emit( 'desconectou', path );
 	});
 }
 
