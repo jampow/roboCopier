4ecb09b1f1207c8a090b46901d0e0ca3204f1eeb
Gianpaulo <jam_pow@hotmail.com>
2014-08-07 19:58:26 -0300

    adicionando módulo que faz as cópias de arquivos e pastas

--- copier.js
+++ copier.js
@@ -1,4 +1,6 @@
 var events = require( 'events' );
+var fse = require( 'fs-extra' );
+var path = require( 'path' );
 var Copier = function(){ events.EventEmitter.call( this ); };
 
 // extendendo EventEmitter
@@ -9,5 +11,33 @@ Copier.prototype = Object.create( events.EventEmitter.prototype, {
 		enumerable: false
 	}
 });
+
+Copier.prototype.validateParentPath = function( filePath ){
+	fse.exists( path.dirname( filePath ), function( exists ){
+		return exists ? true : false;
+	});
+};
+
+Copier.prototype.prepareDestinationPath = function( filePath ){
+	var self = this;
+
+	if( !self.validateParentPath( filePath ) )
+		fse.mkdirsSync( path.dirname( filePath ) );
+}
+
+Copier.prototype.copyFile = function( from, to, callback ){
+	var self = this;
+
+	self.prepareDestinationPath( to );
+
+	fse.copy( from, to, function( err ){
+		if ( err )
+			self.emit( 'copyError', err );
+		else
+			self.emit( 'copySuccess', { "from": from, "to": to });
+
+		if ( callback ) callback( err, from, to );
+	})
+};
 
 module.exports = new Copier();
