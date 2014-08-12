0cc66b43a75fea95d658b5bacc9e30ed209d0bc4
Gianpaulo <jam_pow@hotmail.com>
2014-08-12 12:32:57 -0300

    adicionando módulo de cópia recursiva

--- modules/copier.js
+++ modules/copier.js
@@ -25,17 +25,63 @@ Copier.prototype.prepareDestinationPath = function( filePath ){
 		fse.mkdirsSync( path.dirname( filePath ) );
 }
 
-Copier.prototype.copyFile = function( from, to ){
+Copier.prototype.copyFile = function( from, to, callback ){
 	var self = this;
+	var error = null;
 
 	self.prepareDestinationPath( to );
 
 	fse.copy( from, to, function( err ){
-		if ( err )
+		if ( err ) {
 			self.emit( 'copyError', err );
-		else
+			error = err;
+		} else {
 			self.emit( 'copySuccess', { "from": from, "to": to });
-	})
+		}
+	});
+
+	if ( callback ) callback( error );
+};
+
+Copier.prototype.copyFileRecursive = function( from, to, force ){
+	var self = this;
+
+	var force = force || false;
+
+	var foldersFrom = from.split( path.sep );
+	var fileFrom = foldersFrom.pop();
+
+	var foldersTo = to.split( path.sep );
+	var fileTo = foldersTo.pop();
+
+	var sizeFrom = foldersFrom.length;
+
+	var errors = [];
+	var success = [];
+
+	var i = foldersFrom.length - 1;
+	var j = foldersTo.length - 1;
+
+	for (; i-- && j--;) {
+		filePathFrom =  path.join( foldersFrom.join( path.sep ), fileFrom );
+		filePathTo =  path.join( foldersTo.join( path.sep ), fileTo );
+
+		if ( fse.existsSync( filePathFrom ) ){
+			self.copyFile( filePathFrom, filePathTo, function( err ){
+				if ( err ) errors.push( err );
+			});
+
+			if ( !force ) return;
+		}
+
+		foldersFrom.pop();
+		foldersTo.pop();
+	}
+
+	if ( errors.length > 0 )
+		self.emit( 'recursiveCopyError', { 'errors': errors, 'success': success } );
+	else
+		self.emit( 'recursiveCopySuccess', success );
 };
 
 module.exports = new Copier();
--- test/copy.js
+++ test/copy.js
@@ -7,13 +7,17 @@ var assert = require( 'assert');
 // exec( 'mkdir -p from/site{a,b,c}/some/long/path{1,2,3}' );
 // exec( 'touch from/site{a,b,c}/test{a,b,c}.txt' );
 // exec( 'touch from/site{a,b,c}/some/test{a,b,c}.txt ' );
+// exec( 'touch from/site{a,b,c}/some/config.txt' );
 // exec( 'touch from/site{a,b,c}/some/long/test{a,b,c}.txt' );
+// exec( 'touch from/site{a,b,c}/some/long/config.txt' );
 // exec( 'touch from/site{a,b,c}/some/long/path{1,2,3}/test{a,b,c}.txt' );
 
 exec( 'mkdir -p from/sitea/some/long/path1 from/sitea/some/long/path2 from/sitea/some/long/path3 from/siteb/some/long/path1 from/siteb/some/long/path2 from/siteb/some/long/path3 from/sitec/some/long/path1 from/sitec/some/long/path2 from/sitec/some/long/path3' );
 exec( 'touch from/sitea/testa.txt from/sitea/testb.txt from/sitea/testc.txt from/siteb/testa.txt from/siteb/testb.txt from/siteb/testc.txt from/sitec/testa.txt from/sitec/testb.txt from/sitec/testc.txt' );
 exec( 'touch from/sitea/some/testa.txt from/sitea/some/testb.txt from/sitea/some/testc.txt from/siteb/some/testa.txt from/siteb/some/testb.txt from/siteb/some/testc.txt from/sitec/some/testa.txt from/sitec/some/testb.txt from/sitec/some/testc.txt' );
+exec( 'touch from/sitea/some/config.txt from/siteb/some/config.txt from/sitec/some/config.txt' );
 exec( 'touch from/sitea/some/long/testa.txt from/sitea/some/long/testb.txt from/sitea/some/long/testc.txt from/siteb/some/long/testa.txt from/siteb/some/long/testb.txt from/siteb/some/long/testc.txt from/sitec/some/long/testa.txt from/sitec/some/long/testb.txt from/sitec/some/long/testc.txt' );
+exec( 'touch from/sitea/some/long/config.txt from/siteb/some/long/config.txt from/sitec/some/long/config.txt' );
 exec( 'touch from/sitea/some/long/path1/testa.txt from/sitea/some/long/path1/testb.txt from/sitea/some/long/path1/testc.txt from/sitea/some/long/path2/testa.txt from/sitea/some/long/path2/testb.txt from/sitea/some/long/path2/testc.txt from/sitea/some/long/path3/testa.txt from/sitea/some/long/path3/testb.txt from/sitea/some/long/path3/testc.txt from/siteb/some/long/path1/testa.txt from/siteb/some/long/path1/testb.txt from/siteb/some/long/path1/testc.txt from/siteb/some/long/path2/testa.txt from/siteb/some/long/path2/testb.txt from/siteb/some/long/path2/testc.txt from/siteb/some/long/path3/testa.txt from/siteb/some/long/path3/testb.txt from/siteb/some/long/path3/testc.txt from/sitec/some/long/path1/testa.txt from/sitec/some/long/path1/testb.txt from/sitec/some/long/path1/testc.txt from/sitec/some/long/path2/testa.txt from/sitec/some/long/path2/testb.txt from/sitec/some/long/path2/testc.txt from/sitec/some/long/path3/testa.txt from/sitec/some/long/path3/testb.txt from/sitec/some/long/path3/testc.txt' );
 
 // beforeEach( function(){
@@ -33,7 +37,7 @@ describe( 'Teste de cópias dos arquivos', function(){
 			copier.removeAllListeners( 'copyError' );
 
 			copier.on( 'copySuccess',function( data ){
-				if( !fs.existsSync( '/home/gsoares' ) ){
+				if( !fs.existsSync( 'to/sitea/testa.txt' ) ){
 					throw( 'Arquivo não existe' );
 				}
 				done();
@@ -72,6 +76,47 @@ describe( 'Teste de cópias dos arquivos', function(){
 			});
 
 			copier.copyFile( 'from/sitea/some/long/path1', 'to/sitea/some/long/path1' );
+		});
+
+		it( 'Deve procurar o arquivo recursivamente e parar após copiar o primeiro arquivo encontrado', function( done ){
+			copier.removeAllListeners( 'copySuccess' );
+			copier.removeAllListeners( 'copyError' );
+
+			copier.on( 'copySuccess',function( data ){
+				if( !fs.existsSync( data.to ) ) {
+					throw( 'Arquivo não existe' );
+				}
+				done();
+			});
+
+			copier.on( 'copyError',function( data ){
+				throw( 'Erro ao copiar o arquivo' );
+				done();
+			});
+
+			copier.copyFileRecursive( 'from/sitea/some/long/path1/config.txt', 'to/sitea/some/long/path1/config.txt' );
+
+		});
+
+		it( 'Deve procurar o arquivo recursivamente e continuar copiando até passar por todos as pastas', function( done ){
+			copier.removeAllListeners( 'copySuccess' );
+			copier.removeAllListeners( 'copyError' );
+
+			copier.on( 'recursiveCopySuccess',function( data ){
+				for (var i = data.length - 1; i >= 0; i--) {
+					if( !fs.existsSync( data[i] ) )
+						throw( 'Arquivo não existe' );
+				}
+				done();
+			});
+
+			copier.on( 'recursiveCopyError',function( data ){
+				throw( 'Erro ao copiar o arquivo' );
+				done();
+			});
+
+			copier.copyFileRecursive( 'from/sitea/some/long/config.txt', 'to/sitea/some/long/config.txt', true );
+
 		});
 	});
 });
